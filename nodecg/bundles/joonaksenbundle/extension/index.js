37c4c81741c7a926066ac507d3b0201bcf0ea08f
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-09-26 15:04:03 +0300

    myös roolin vaihto

--- nodecg/bundles/joonaksenbundle/extension/index.js
+++ nodecg/bundles/joonaksenbundle/extension/index.js
@@ -1,4 +1,4 @@
 module.exports = function (nodecg) {
-	nodecg.Replicant("speakerReplicant", { defaultValue: "Maija Mehiläinen" });
-
+  nodecg.Replicant('speakerReplicant', { defaultValue: 'Maija Mehiläinen' });
+  nodecg.Replicant('titleReplicant', { defaultValue: 'UX Specialist' });
 };
